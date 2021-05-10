8ef456f2f35d35c9577c09ecc87f1bf8bb0d7b24
Bojan <bojan.brunet@gmail.com>
2021-05-10 21:33:46 +0200

    firstCommit

---
+++ src/main/java/com/webprojteretana/WebProjTeretana2021/WebProjTeretana2021Application.java
@@ -0,0 +1,13 @@
+package com.webprojteretana.WebProjTeretana2021;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebProjTeretana2021Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebProjTeretana2021Application.class, args);
+	}
+
+}
