537da066e53091f6cb3e4d6d925a9fc5f2cce52d
Bojan <bojan.brunet@gmail.com>
2021-06-18 01:06:21 +0200

    dodao funkcionalnosti vezane za termin, treba jos doraditi funkcionalnost dobabaljanja termina

---
+++ src/main/java/com/webprojteretana/WebProjTeretana2021/service/impl/TerminServiceImpl.java
@@ -0,0 +1,11 @@
+package com.webprojteretana.WebProjTeretana2021.service.impl;
+
+
+import com.webprojteretana.WebProjTeretana2021.service.TerminService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class TerminServiceImpl implements TerminService {
+
+
+}
