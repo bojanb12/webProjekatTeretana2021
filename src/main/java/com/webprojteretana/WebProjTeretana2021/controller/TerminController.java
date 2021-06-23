d4546bd1497266e8cdbdedd1a1a95a29a3abeaa1
Bojan <bojan.brunet@gmail.com>
2021-06-23 23:54:51 +0200

    dobavljanje termina za pregled controller

--- src/main/java/com/webprojteretana/WebProjTeretana2021/controller/TerminController.java
+++ src/main/java/com/webprojteretana/WebProjTeretana2021/controller/TerminController.java
@@ -1,7 +1,9 @@
 package com.webprojteretana.WebProjTeretana2021.controller;
 
+import com.webprojteretana.WebProjTeretana2021.entity.Clan;
 import com.webprojteretana.WebProjTeretana2021.entity.FitnesCentar;
 import com.webprojteretana.WebProjTeretana2021.entity.Termin;
+import com.webprojteretana.WebProjTeretana2021.entity.dto.ClanDTO;
 import com.webprojteretana.WebProjTeretana2021.entity.dto.FitnesCentarDTO;
 import com.webprojteretana.WebProjTeretana2021.entity.dto.TerminDTO;
 import com.webprojteretana.WebProjTeretana2021.service.FitnesCentarService;
@@ -10,10 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value="/api/termini")
@@ -37,5 +36,18 @@ public class TerminController {
         TerminDTO newTerminDTO = new TerminDTO(newTermin.getVreme(), newTermin.getDan(), newTermin.getCena(), newTermin.getSalaTermin(), newTermin.getTreningTermin());
 
         return new ResponseEntity<>(newTerminDTO, HttpStatus.OK);
+    }
+
+    //dobavljanje termina za pregled
+    @GetMapping(
+            value = "/{id}",
+            produces = MediaType.APPLICATION_JSON_VALUE)  // tip odgovora
+    public ResponseEntity<TerminDTO> pregledTermina(@PathVariable(name = "id") Long id) {
+
+        Termin termin = this.terminService.findOne(id);
+
+        TerminDTO terminDTO = new TerminDTO(termin.getId(), termin.getVreme(), termin.getDan(), termin.getCena(), termin.getSalaTermin(), termin.getTreningTermin());
+
+        return new ResponseEntity<>(terminDTO, HttpStatus.OK);
     }
 }
