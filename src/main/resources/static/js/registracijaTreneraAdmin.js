f15fb5353c168063fa8cb1b2df61ada8037c3e59
Bojan <bojan.brunet@gmail.com>
2021-07-08 01:00:21 +0200

    registracija trenera admin funkcionalnost uradjena

---
+++ src/main/resources/static/js/registracijaTreneraAdmin.js
@@ -0,0 +1,53 @@
+$(document).on("submit", "form", function(event){
+
+    // da se izbegne izvrsavanja pravog submita forme
+    event.preventDefault();
+
+    var korisnickoIme = $("#korisnickoIme").val();
+    var lozinka = $("#lozinka").val();
+    var ime = $("#ime").val();
+    var prezime = $("#prezime").val();
+    var brojTel = $("#brojTel").val();
+    var email = $("#email").val();
+    var adresa = $("#adresa").val();
+    var datumRodjenja = $("#datumRodjenja").val();
+    var aktivan = true;
+    var uloga = 'Trener';
+
+    //var danas = new Date(); // datum registracije
+
+    var noviJSON = formToJSON(korisnickoIme, lozinka, ime, prezime, brojTel, email, adresa, datumRodjenja, aktivan, uloga);
+
+    $.ajax({
+            type: "POST",
+            url: "http://localhost:8083/api/treneri/registration",
+            dataType: "json",
+            contentType: "application/json",
+            data: noviJSON,
+            success: function () {
+                 alert("Nalog trenera " + ime + " " + prezime + " je uspešno kreiran!");
+                 window.location.href = "treneri.html";
+                 },
+            error: function (data) {
+                 alert("Greška!");
+            }
+    });
+});
+
+function formToJSON(korisnickoIme, lozinka, ime, prezime, brojTel, email, adresa, datumRodjenja, aktivan, uloga){
+    return JSON.stringify(
+        {
+        "korisnickoIme": korisnickoIme,
+        "lozinka": lozinka,
+        "ime": ime,
+        "prezime": prezime,
+        "brojTel": brojTel,
+        "email": email,
+        "adresa": adresa,
+        "datumRodjenja": datumRodjenja,
+        "aktivan" : aktivan,
+        "uloga" : uloga
+        //"danas": danas,
+        }
+    );
+};
